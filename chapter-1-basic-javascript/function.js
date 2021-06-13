60d4898b1f3be7a0a63e6a940bdbfae0d66f734a
codershuvo <nisho.mushfiq2020@gmail.com>
2021-06-14 01:58:39 +0600

    function

--- chapter-1-basic-javascript/function.js
+++ chapter-1-basic-javascript/function.js
@@ -8,4 +8,20 @@ var funcName = function () {
 
 var newName = funcName(); 
 
+function helloPrint () {
+    console.log('Hello I am from the function'); 
+}
+helloPrint();
 
+
+var helloWorld = function () {
+    console.log('Hello World!'); 
+}
+helloWorld();
+
+
+function sum (a, b) {
+    return a + b; 
+}
+console.log(sum(5, 15)); 
+console.log(sum(13, 28));
