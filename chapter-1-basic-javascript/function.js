0137fd32b00d1b92d47b091004ea53acffea03f6
codershuvo <nisho.mushfiq2020@gmail.com>
2021-06-18 00:17:30 +0600

    callback added

--- chapter-1-basic-javascript/function.js
+++ chapter-1-basic-javascript/function.js
@@ -25,3 +25,23 @@ function sum (a, b) {
 }
 console.log(sum(5, 15)); 
 console.log(sum(13, 28));
+
+
+
+function printMyName (name, age) {
+    console.log('My name is: ' + name + ' and I am ' + age + ' years old!')
+}
+printMyName('mosfiqur rahman', 25); 
+
+
+
+
+function callMyName (name, callback) {
+    var myAge = 20; 
+    callback(myAge);
+    console.log('It is interesting ? Yes it is Mr. ' + name); 
+}
+function hello (age) { 
+    console.log('I am passed through argument and my age is: ' + age); 
+}
+callMyName('Mosfiqur Rahman', hello); 
