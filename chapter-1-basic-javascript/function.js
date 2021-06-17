5eccbea4cb2933ad967c8ab1c93b141161f6e239
codershuvo <nisho.mushfiq2020@gmail.com>
2021-06-18 00:23:58 +0600

    function

--- chapter-1-basic-javascript/function.js
+++ chapter-1-basic-javascript/function.js
@@ -45,3 +45,26 @@ function hello (age) {
     console.log('I am passed through argument and my age is: ' + age); 
 }
 callMyName('Mosfiqur Rahman', hello); 
+
+
+
+function welcomeMsg (name) {
+    console.log('Welcome Mr. ' + name); 
+    return function option (menu) {
+        console.log('Do you like ' + menu + ' Mr. ' + name); 
+    }
+}
+welcomeMsg('Mosfiqur Rahman') ('Coffee'); 
+
+
+
+function changeValue (a) {
+    console.log('Value of a inside function before changing: ' + a); 
+    a = 10; 
+    console.log('Value of a inside function after changing: ' + a); 
+    return a;
+}
+var a = 100; 
+console.log('Value of a before changing function applied: ' + a); 
+changeValue(a); 
+console.log('Value of a after changing function is applied: ' + a); 
