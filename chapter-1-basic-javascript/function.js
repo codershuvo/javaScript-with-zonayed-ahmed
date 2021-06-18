8891c97531654746713553be7afa1bd4b2b05c7d
codershuvo <nisho.mushfiq2020@gmail.com>
2021-06-19 00:21:11 +0600

    late night

--- chapter-1-basic-javascript/function.js
+++ chapter-1-basic-javascript/function.js
@@ -15,11 +15,11 @@ var funName = function() {
 funName(); 
 
 
-function aFunc(parameters) {
-    // do something with the parameters
-    // return the result
-}
-aFunc(arguments); 
+// function aFunc(parameters) {
+//     // do something with the parameters
+//     // return the result
+// }
+// aFunc(arguments); 
 
 
 function sumMachine(a, b) {
@@ -28,4 +28,66 @@ function sumMachine(a, b) {
 }
 console.log(sumMachine(2, 2)); 
 console.log(sumMachine(4, 4)); 
+
+
+function printMyName(name, age) {
+    console.log('My name is ' + name + ' and I am ' + age + ' years old.'); 
+}
+printMyName('Mosfiqur Rahman', 24); 
+
+
+
+function callMyName(name, callback) {
+    var myAge = 20; 
+    callback(myAge);
+    console.log('Is it interesting? Yes it is Mr. ' + name); 
+}
+function hello(age) {
+    console.log('I am passed through argument and my age is: '+ age); 
+}
+callMyName('Mosfiqur Rahman', hello); 
+
+
+
+
+function welcomeMsg(name) {
+    console.log('Welcome Mr. ' + name); 
+    return function options(menu) {
+        console.log('Do you like ' + menu + ' Mr. ' + name); 
+    }
+}
+welcomeMsg('Mosfiqur Rahman') ('Coffee'); 
+
+
+
+
+
+function changeValue(a) {
+    console.log('Value of a inside function before changing: ' + a); 
+    a = 10; 
+    console.log('Value of a inside function after changing: ' + a); 
+    return a; 
+}
+var a = 100; 
+console.log('Value of a before changing function is applied: ' + a); 
+changeValue(a); 
+console.log('Value of a after changing function is applied: ' + a); 
+
+
+
+
+
+
+
+function aFunc(a) {
+    console.log('Before changing th property: ' + a.one); 
+    a.one = 'two'; 
+    console.log('After changing the property: ' + a.one); 
+}
+var a = {
+    one: 'One'
+}
+aFunc(a); 
+
+
 
