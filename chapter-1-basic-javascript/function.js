9af2a38f6c764d73ff8056bea32b3222090ea6c8
codershuvo <nisho.mushfiq2020@gmail.com>
2021-06-19 00:01:50 +0600

    function start

--- chapter-1-basic-javascript/function.js
+++ chapter-1-basic-javascript/function.js
@@ -1,70 +1,31 @@
-function funcName () {
+console.log(11); 
+
+function funcName() {
     // Statement
-}
-
-var funcName = function () {
-    // Statement 
-}
-
-var newName = funcName(); 
-
-function helloPrint () {
+    // function statement
     console.log('Hello I am from the function'); 
 }
-helloPrint();
+funcName(); 
 
-
-var helloWorld = function () {
-    console.log('Hello World!'); 
+var funName = function() {
+    // Statement
+    // function expression
+    console.log('Hello I am from the function'); 
 }
-helloWorld();
+funName(); 
 
 
-function sum (a, b) {
-    return a + b; 
+function aFunc(parameters) {
+    // do something with the parameters
+    // return the result
 }
-console.log(sum(5, 15)); 
-console.log(sum(13, 28));
+aFunc(arguments); 
 
 
-
-function printMyName (name, age) {
-    console.log('My name is: ' + name + ' and I am ' + age + ' years old!')
+function sumMachine(a, b) {
+    var sum = a + b;
+    return sum; 
 }
-printMyName('mosfiqur rahman', 25); 
+console.log(sumMachine(2, 2)); 
+console.log(sumMachine(4, 4)); 
 
-
-
-
-function callMyName (name, callback) {
-    var myAge = 20; 
-    callback(myAge);
-    console.log('It is interesting ? Yes it is Mr. ' + name); 
-}
-function hello (age) { 
-    console.log('I am passed through argument and my age is: ' + age); 
-}
-callMyName('Mosfiqur Rahman', hello); 
-
-
-
-function welcomeMsg (name) {
-    console.log('Welcome Mr. ' + name); 
-    return function option (menu) {
-        console.log('Do you like ' + menu + ' Mr. ' + name); 
-    }
-}
-welcomeMsg('Mosfiqur Rahman') ('Coffee'); 
-
-
-
-function changeValue (a) {
-    console.log('Value of a inside function before changing: ' + a); 
-    a = 10; 
-    console.log('Value of a inside function after changing: ' + a); 
-    return a;
-}
-var a = 100; 
-console.log('Value of a before changing function applied: ' + a); 
-changeValue(a); 
-console.log('Value of a after changing function is applied: ' + a); 
