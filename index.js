b41f74871881a51a81975600e71089b155ef7c9f
mosfiqurRahmanJsd <nisho.mushfiq2020@gmail.com>
2021-08-04 08:56:33 +0600

    variable and data type

---
+++ index.js
@@ -0,0 +1,80 @@
+// output / print
+console.log('Hello world from Inside!');
+
+console.log('Wrong!'); 
+
+// variable naming 
+var person = 'Mosfiqur Rahman'; 
+var some_thing = 'Anything'; 
+var _canI = 'Yes'; 
+var $text = 'I am Valid!'; 
+var my_name = 'Mosfiqur'; 
+var myName = 'Shuvo'; 
+
+// camelCase Format
+var camelCase; 
+var myName; 
+var myAge; 
+var myID; 
+var formToHandle; 
+var studentCount; 
+var totalScore; 
+var numberOfPeople
+var isValidate; 
+
+
+var myName = 'Zonayed'; 
+var author = myName; 
+console.log(author);
+
+
+
+// data type : 1) Primitive Data Type, 2) Non-primitive Data Type
+// 1) Primitive Data Type:
+// number:
+var aNumber = 10; 
+var anotherNumber = 10.10; 
+
+// string:
+var text = 'I want to say something'; 
+var text2 = "This is exactly the same way, but use either one"; 
+var isString = `10`;   // is also a string, not a number
+
+// boolean:
+var isTrue = true; 
+var isFalse = false; 
+
+
+// undefined: 
+var someThing; 
+
+
+// null: 
+var anyThing = null;
+
+
+// concatenation
+var text = 'how' + ' are you?'; 
+var welComeMsg = 'Hello, ' + text; 
+console.log(typeof welComeMsg);
+
+
+var number = 10 + 10; 
+var isString = 'Hello there ' + 20; 
+
+var complex = 10 + 10 + 'Some Text' + 10; 
+console.log( complex);
+
+
+// data change 
+var personName = 'Zonayed Ahmed'; 
+personName = 'CSE Engineer Zonayed';
+personName = 'shuvo';
+
+var someThing = 'Zonayed Ahmed'; 
+someThing = 10;
+
+
+
+
+// 2) Non-primitive Data Type:
