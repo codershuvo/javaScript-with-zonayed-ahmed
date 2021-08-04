08c3da852aceb54156348d23c36ece04622bf677
mosfiqurRahmanJsd <nisho.mushfiq2020@gmail.com>
2021-08-04 10:23:15 +0600

    variable types operator naming etc

--- variable-types-operator.js
+++ variable-types-operator.js
@@ -75,6 +75,84 @@ var someThing = 'Zonayed Ahmed';
 someThing = 10;
 
 
+// single line comment
+var myName = 'Zonayed Ahmed'; // your comment here
+// multi line comment
+/*
+your comment here
+*/
+/*
+Author: Mosfiqur Rahman
+Email: mosfiqur.rahman.jsd@gmail.com
+websire: https://www.mosfiqur.web.app
+*/
+
+
+
+
+// Operator:
+// Arithmentic Operator: (+, -, *, /, %, ++, --)
+var a = 20; 
+var b = 7; 
+console.log(a + b); 
+console.log(a-b);
+console.log(a*b); 
+console.log(a/b);
+console.log(a%b);
+
+var x = 10; 
+x++; 
+console.log(x);
+var p = 7; 
+p--; 
+console.log(p);
+
+// Comparison operator: (==, !=, >, <, >=, <=, ===, !==)
+var a = 21; 
+var b = 20; 
+console.log(a <= b);
+
+
+// logical operator: (&&, ||, !)
+var c = 10; 
+var d = 20; 
+var result = !(c > d);
+console.log(result);
+
+// Assignment Operator:(+=, -=, *=, %=)
+var a = 10; 
+a %= 5; 
+console.log(a);
+
+// conditional operator: (... ? ... : ...)
+var a = 10; 
+var b = 20; 
+var c = a > b ? 100 : 200; 
+console.log(c);
+
+
+// others operator: (typeof)
+var a = 10; 
+typeof(a); 
+console.log(typeof a);
+
+
+// typeof return types
+/*
+1. number
+2. string
+3. boolean
+4. object
+5. function
+6. undefined
+7. null
+*/
+
+
+
+
+
+
 
 
 // 2) Non-primitive Data Type:
