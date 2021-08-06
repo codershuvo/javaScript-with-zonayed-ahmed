94739b03553e871d740c487d3d8a36a2bce8a4d3
mosfiqurRahmanJsd <nisho.mushfiq2020@gmail.com>
2021-08-07 00:39:47 +0600

    dry added

---
+++ chapter-1-basic/dry.js
@@ -0,0 +1,22 @@
+function addTwoNum(x, y) {
+    var sum = x + y; 
+    console.log('Sum of ' + x + ' and ' + y + ' is: ' + sum); 
+}
+
+console.log(addTwoNum(5, 10)); 
+console.log(addTwoNum(20, 30)); 
+console.log(addTwoNum(40, 50)); 
+console.log(addTwoNum(60, 70)); 
+console.log(addTwoNum(80, 90)); 
+
+
+
+function welcomeMsg(name) {
+    console.log('Welcome Mr. ' + name); 
+}
+
+welcomeMsg('zonayed'); 
+welcomeMsg('mosfiqur'); 
+welcomeMsg('ramzan'); 
+welcomeMsg('sagar'); 
+welcomeMsg('miraj'); 
