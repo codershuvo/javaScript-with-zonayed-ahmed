92b39539f30fb6e213285a6155e8d8693afaa4b3
mosfiqurRahmanJsd <nisho.mushfiq2020@gmail.com>
2021-08-06 23:20:05 +0600

    statement or expression

---
+++ chapter-1-basic/statement-or-expression.js
@@ -0,0 +1,34 @@
+// expression
+//  10 + 10
+// 10*20
+
+// statement 
+var assignValue = 10 + 10; 
+
+if(10 < 20) {
+    console.log('Yeah! It is true'); 
+} else {
+    console.log('No! It is not True')
+}
+
+
+
+// for ( // expressions) {
+//     // Some code here
+// }
+do {
+    // some code here
+} while (expression) {
+    // Some code here
+}
+
+
+var someFunc = function(params) {
+    // some code here
+}
+
+
+// function statement
+function sumFunc(params) {
+    // some code here
+}
