f1f2a961d6fd5b4db1a7ee2ccb2969d7b399689c
mosfiqurRahmanJsd <nisho.mushfiq2020@gmail.com>
2021-08-09 02:56:56 +0600

    chapter 2 finished

---
+++ chapter-2-javascript-advance/strict.js
@@ -0,0 +1,8 @@
+'use strict'; 
+var name = 'mosfiqur rahman';
+var age = 10; 
+var address = 'Rajshahi'; 
+console.log('My name is ' + name + ' and I am ' + age + ' years old!'); 
+function aFunc() {
+    console.log('I am a ' + job + ' And I am from ' + address); 
+}
