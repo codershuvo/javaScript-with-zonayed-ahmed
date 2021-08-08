73e3809afd1b83f26b304aa4dfd7e5acd99f3ffd
mosfiqurRahmanJsd <nisho.mushfiq2020@gmail.com>
2021-08-09 02:09:59 +0600

    wrapper

---
+++ chapter-2-javascript-advance/wrapper-fun.js
@@ -0,0 +1,14 @@
+var str = 'My name is Mosfiqur Rahman'; 
+console.dir(str);
+
+
+var str2 = new String('My name is Shuvo'); 
+console.dir(str2);
+
+
+var num = new Number(10); 
+console.dir(num); 
+
+
+var bool = new Boolean(true); 
+console.dir(bool); 
